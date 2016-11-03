478e26fba95cd4478e75bf1d4bf9f124772c4af3
PowerGamer1 <PowerGamer1@users.noreply.github.com>
2016-11-03 10:51:40 +0200

    Fixes #96, fixes #157: Fix active status of multilevel submenu items

--- Nav.php
+++ Nav.php
@@ -106,7 +106,7 @@ class Nav extends Widget
      */
     public $dropDownCaret;
     /**
-     * @var string name of a class to use for rendering dropdowns withing this widget. Defaults to [[Dropdown]].
+     * @var string name of a class to use for rendering dropdowns within this widget. Defaults to [[Dropdown]].
 	 * @since 2.0.7
      */
 	public $dropdownClass = 'yii\bootstrap\Dropdown';
@@ -239,6 +239,15 @@ class Nav extends Widget
             if (ArrayHelper::remove($items[$i], 'active', false) || $this->isItemActive($child)) {
                 Html::addCssClass($items[$i]['options'], 'active');
                 if ($this->activateParents) {
+                    $active = true;
+                }
+            }
+            $childItems = ArrayHelper::getValue($child, 'items');
+            if(is_array($childItems)) {
+                $activeParent = false;
+                $items[$i]['items'] = $this->isChildActive($childItems, $activeParent);
+                if ($activeParent) {
+                    Html::addCssClass($items[$i]['options'], 'active');
                     $active = true;
                 }
             }
